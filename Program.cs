76292529d5a5181d24863465733e5107bf02564c
Axel Brinck <axel.louis.1991@gmail.com>
2020-12-07 23:06:25 +0100

    updated doc

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using MaximumSpeed.Utils;
 
 namespace MaximumSpeed
@@ -20,7 +23,7 @@ namespace MaximumSpeed
             Chrono.Start();
             using(var writer = new BinaryWriter(File.OpenWrite(fileName)))
             {
-                for (var value = 0L; value < quantity; value++)
+                for (var value = 0m; value < quantity; value++)
                 {
                     writer.Write(value);
                 }
@@ -38,6 +41,9 @@ namespace MaximumSpeed
             var array = new long[elementsToBuffer];
             var arraySize = array.Length * sizeof(long);
             var elementSize = sizeof(long);
+            var converters = new List<Task>();
+            var processors = Environment.ProcessorCount;
+            var semaphore = new Semaphore(processors - 4, processors - 4);
             
             Chrono.Start();
             using(var reader = new BinaryReader(File.OpenRead(FileName)))
@@ -46,15 +52,28 @@ namespace MaximumSpeed
                 {
                     var bytesToRead = position + arraySize >= fileSize ? (int)(fileSize - position) : arraySize;
                     var elementsToRead = bytesToRead / elementSize;
-                    byte[] buffer = reader.ReadBytes(bytesToRead);
-                    
-                    for (var i = 0; i < elementsToRead; i++)
-                    {
-                        fixed(byte *pBuffer = buffer)
+                    var buffer = reader.ReadBytes(bytesToRead);
+                    Thread.CurrentThread.Priority = ThreadPriority.Highest;
+                    var task = Task.Factory.StartNew(() => {
+                        //Console.WriteLine("Task started");
+                        var array = new decimal[elementsToRead];
+                        for (var i = 0; i < elementsToRead; i++)
                         {
-                            array[i] = *(long*) (pBuffer + i * elementSize);
+                            semaphore.WaitOne();
+                            fixed(byte *pBuffer = buffer)
+                            {
+                                array[i] = new decimal(new int[] {
+                                    *(int*) (pBuffer + i * elementSize),
+                                    *(int*) (pBuffer + i * elementSize + 4),
+                                    *(int*) (pBuffer + i * elementSize + 8),
+                                    *(int*) (pBuffer + i * elementSize + 12)
+                                });
+                            }
+                            semaphore.Release();
                         }
-                    }
+                        //Console.WriteLine("Task ended");
+                    });
+                    converters.Add(task);
 
                     position += bytesToRead;
                 }
@@ -62,19 +81,20 @@ namespace MaximumSpeed
             }
             Chrono.End();
 
+            Task.WhenAll(converters);
+
             Chrono.Bytes = new FileInfo(fileName).Length;
             Console.WriteLine($"Read Speed: {Chrono.GetBytesPerSecond():0.000}Mb/s. Total: {Chrono.Bytes} bytes.");
+            Console.WriteLine($"Read Speed: {Chrono.GetBytesPerSecond() / 16:0.000}MDecimals/s. Total: {Chrono.Bytes} bytes.");
         }
 
         static unsafe void Main(string[] args)
         {
-            File.Delete(FileName);
+            //File.Delete(FileName);
             
-            WriteDummyFile(FileName, TotalValues);
+            //WriteDummyFile(FileName, TotalValues);
 
-            ReadDummyFile(FileName, 4096 / sizeof(long));
-
-            
+            ReadDummyFile(FileName, (1024 * 1024) / sizeof(decimal));
         }
     }
 }
